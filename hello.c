24940a82ffa08b3a3efed1ea36497e2fb6585605
ravindrapsr <ravindrapratap95@gmail.com>
2026-06-03 16:04:04 +0530

    One line removed

--- hello.c
+++ hello.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 int main() {
     printf("Hello, World!..I am trying Github\n");
-    printf("Code Modified..One line added\n");
-    printf("Code Modified..One more line added\n");
+    printf("Code Modified..One line removed\n");
     return 0;
 }
