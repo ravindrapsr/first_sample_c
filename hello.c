62f16274d99d698ce397267bb50c326e8f460c04
ravindrapsr <ravindrapratap95@gmail.com>
2026-06-03 17:44:04 +0530

    Modify hello.c to add and remove print statementsGithub editor try

--- hello.c
+++ hello.c
@@ -2,5 +2,6 @@
 int main() {
     printf("Hello, World!..I am trying Github\n");
     printf("Code Modified..One line removed\n");
+    printf("Code Modified..One line added on Github editor\n");
     return 0;
 }
